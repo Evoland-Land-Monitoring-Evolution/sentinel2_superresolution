c79b93266106f932abc557b7a706fbbe49b6136a
Julien Michel <julien.michel@cesbio.cnes.fr>
2024-05-23 15:11:22 +0200

    BUG: Attempt to fix packages bug

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ from setuptools import setup
 
 if __name__ == "__main__":
     try:
-        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+        setup(
+            use_scm_version={"version_scheme": "no-guess-dev"},
+            include_package_data=True,
+        )
     except:  # noqa
         print(
             "\n\nAn error occurred while building the project, "
